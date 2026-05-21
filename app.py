b6242fbe9d26b8e73a248240b7c206009cb94ee4
LorenzoNS <12402133@aluno.cotemig.com.br>
2026-05-21 09:42:28 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,10 +1,38 @@
-from flask import Flask, render_template
+from flask import Flask, request, render_template_string
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return render_template('home.html')
+def show_the_login_form():
+    return render_template_string("""
+        <h2>Login</h2>
+        <form method="POST">
+            <input type="text" name="usuario" placeholder="Usuário"><br><br>
+            <input type="password" name="senha" placeholder="Senha"><br><br>
+            <button type="submit">Entrar</button>
+        </form>
+    """)
 
-if __name__ == '__main__':
+def do_the_login():
+    usuarios = ["lorenzo", "janaina", "dolga", "antonio"]
+    senhas = ["12402133", "cotemig2026", "cotemig2026", "cotemig2026"]
+
+    usuario = request.form.get('usuario')
+    senha = request.form.get('senha')
+
+    if usuario in usuarios:
+        if senha == senhas[usuarios.index(usuario)]:
+            return f"<h1>Bem-vindo(a), {usuario}!</h1>"
+        else:
+            return "<h1>Login inválido</h1>"
+    else:
+        return "<h1>Login inválido</h1>"
+
+@app.route('/', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        return do_the_login()
+    else:
+        return show_the_login_form()
+
+if __name__ == "__main__":
     app.run(debug=True)
